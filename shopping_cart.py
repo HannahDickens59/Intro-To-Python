8dde3389d5c934d9a474b34ef2b57319b19638a5
Hannah Dickens <hannahdickens07@gmail.com>
2025-06-26 21:05:24 +0200

    Update shopping_cart.py

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,32 +1,38 @@
 
-message = 'Bananas = R15.00, Apples = R20.00, Pears = R20.00, Oranges = R30.00, Grapes = R25.00'
+message = 'Bananas = R20.00, Apples = R24.99, Pears = R27.55, Oranges = R29.99, Grapes = R25.00'
 print(message)
 
-my_dict = {'Bananas': 15.00, 'Apples': 20.00, 'Pears': 20.00, 'Oranges': 30.00, 'Grapes': 25.00}
+my_dict = {'Bananas': 20.00, 'Apples': 24.99, 'Pears': 27.55, 'Oranges': 29.99, 'Grapes': 25.00}
 
 final = 00.00
+cart_items = [] # Initialize an empty list to store items bought
 
 num = input("Choose an item to put into your cart: ")
     
 while num != "done":
     if num == "Bananas":
         final += my_dict['Bananas']
+        cart_items.append(num) #Adding item to cart - bananas
         print(f"{num} added. Current total: {final}") # Added feedback
         
     elif num == "Apples":
         final += my_dict['Apples']
+        cart_items.append(num)
         print(f"{num} added. Current total: {final}")
         
     elif num == "Pears":
         final += my_dict['Pears']
+        cart_items.append(num)
         print(f"{num} added. Current total: {final}")
         
     elif num == "Oranges":
         final += my_dict['Oranges']
+        cart_items.append(num)
         print(f"{num} added. Current total: {final}")
         
     elif num == "Grapes":
         final += my_dict['Grapes']
+        cart_items.append(num)
         print(f"{num} added. Current total: {final}")
         
     else:
@@ -35,6 +41,12 @@ while num != "done":
     num = input("Choose another item to put into your cart, or type 'done' to cash out: ")
 
 # Once the loop exits (num is "done")
+
+print("   ")
+print("   --- YOUR CART ITEMS: ---")
+
+print(cart_items)
+print("   ")
 print(f"   --- TOTAL CASH-OUT PRICE: ---")
 print (f"  R{final}")
 
